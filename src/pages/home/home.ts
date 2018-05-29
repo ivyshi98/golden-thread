73eaafea9424d98de438003e6651d2f61d4050b7
“Ivy <ivys725@gmail.com>
2018-05-29 14:20:13 +0200

    Add new pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegistrationPage } from '../registration/registration';
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +13,10 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  navigateToLogin(){
+    this.navCtrl.push(LoginPage);
+  }
+  navigateToRegistration(){
+    this.navCtrl.push(RegistrationPage);
+  }
 }
